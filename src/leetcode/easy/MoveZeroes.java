5562f7cfe465d05377f7a9e293cc5a765e6fc3cb
angelmmc <ammcardenas@hotmail.com>
2026-06-27 12:38:04 -0600

    Move Zeroes
    -Problem solved :D
    -Brain activated

--- src/leetcode/easy/MoveZeroes.java
+++ src/leetcode/easy/MoveZeroes.java
@@ -9,12 +9,10 @@ import java.util.Arrays;
 public class MoveZeroes {
 
     public static void Solution(int[] nums) {
-
         int i = 0;
-        int j = 0;
-        while (i < nums.length && j < nums.length) {
+        int j = 1;
+        while (j < nums.length) {
 
-            j = i++;
             while (j < nums.length && nums[j] == 0) {
                 j++;
             }
@@ -23,21 +21,41 @@ public class MoveZeroes {
                 nums[i] = nums[j];
                 nums[j] = 0;
             }
-
             i++;
-
-
+            j = i;
         }
         System.out.println(Arrays.toString(nums));
     }
 
     static void main() {
-        //int[] nums = {1, 0, 0};
-       // int[] nums2 = {0,1,0,3,12};
-        int [] nums3 = {4,2,4,0,0,3,0,5,1,0};
+        int[] nums1 = {};
+        int[] nums2 = {0};
+        int[] nums3 = {42};
+        int[] nums4 = {0, 0, 0, 0, 0};
+        int[] nums5 = {1, 2, 3, 4, 5};
+        int[] nums6 = {0, 0, 0, 0, 1, 0, 0, 0, 0};
+        int[] nums7 = {0, 0, 1, 2, 3, 0, 0};
+        int[] nums8 = {-1, 0, -2, 0, -3, 0};
+        int[] nums9 = {1, 1, 1, 0, 1, 1, 0, 1};
+        int[] nums10 = {0, 1};
+        int[] nums11 = {1, 0};
+        int[] nums12 = {1, 2, 3, 0, 0, 0, 4};
+        int[] nums13 = {0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 0, 0, 6, 0, 0, 7, 0, 8, 0, 0};
+        int[] nums14 = {0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8};
 
-        //Solution(nums);
-       // Solution(nums2);
+        Solution(nums1);
+        Solution(nums2);
         Solution(nums3);
+        Solution(nums4);
+        Solution(nums5);
+        Solution(nums6);
+        Solution(nums7);
+        Solution(nums8);
+        Solution(nums9);
+        Solution(nums10);
+        Solution(nums11);
+        Solution(nums12);
+        Solution(nums13);
+        Solution(nums14);
     }
 }

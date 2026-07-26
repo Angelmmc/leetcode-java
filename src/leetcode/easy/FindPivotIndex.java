23a6ba1e8bf1f4ea708117d5f44c77f547dc9527
angelmmc <ammcardenas@outlook.com>
2026-07-25 18:15:15 -0600

    724. Find Pivot Index
    -Problem solved.

--- src/leetcode/easy/FindPivotIndex.java
+++ src/leetcode/easy/FindPivotIndex.java
@@ -1,21 +1,42 @@
 package leetcode.easy;
 
+/**
+ * 724.
+ * <a href="https://leetcode.com/problems/find-pivot-index/description/?envType=study-plan-v2&envId=leetcode-75">Find Pivot Index</a>
+ */
 public class FindPivotIndex {
     public static int pivotIndex(int[] nums) {
-
         int[] prefixSum = new int[nums.length + 1];
-
         for (int i = 0; i < nums.length; i++) {
             prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
 
-
-        return prefixSum[6] - prefixSum[5 - 1];
+        for (int i = 1; i <= nums.length; i++) {
+            int leftSum = prefixSum[i - 1];
+            int rightSum = prefixSum[nums.length] - prefixSum[i];
+            if (leftSum == rightSum) {
+                return i - 1;
+            }
+        }
+        return -1;
     }
 
     static void main() {
-        int[] nums = {1, 7, 3, 6, 5, 6};
-        System.out.println(pivotIndex(nums));
+        int[] nums1 = {1, 7, 3, 6, 5, 6};
+        int[] nums2 = {1, 2, 3};
+        int[] nums3 = {2, 1, -1};
+        int[] nums4 = {1, 2, 3, 4, 5};
+        int[] nums5 = {10};
+        int[] nums6 = {0, 0, 0};
+        int[] nums7 = {-1, -1, 0, 1, 1, 0};
 
+
+        System.out.println(pivotIndex(nums1));
+        System.out.println(pivotIndex(nums2));
+        System.out.println(pivotIndex(nums3));
+        System.out.println(pivotIndex(nums4));
+        System.out.println(pivotIndex(nums5));
+        System.out.println(pivotIndex(nums6));
+        System.out.println(pivotIndex(nums7));
     }
 }
